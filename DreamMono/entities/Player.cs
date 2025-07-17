bc3bde96b6176f57c072fb5774ecae1236ee932b
Jbillaba <Jeremyvillalva66@gmail.com>
2025-07-17 11:23:16 -0400

    added:
    added ldtk file

--- DreamMono/entities/Player.cs
+++ DreamMono/entities/Player.cs
@@ -4,7 +4,9 @@ namespace DreamMono.entities;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Graphics;
+using System.Data.Common;
 
 public class Player
 {
+    readonly Player data;
 }
